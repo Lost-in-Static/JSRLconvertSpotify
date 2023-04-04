84e5642edb46b05803151628f0822e991ccbae8d
Lost-in-Static <32521625+Lost-in-Static@users.noreply.github.com>
2023-04-04 15:01:02 +0000

    List playlist names in Import

--- src/pages/JSRLiveImport.tsx
+++ src/pages/JSRLiveImport.tsx
@@ -1,11 +1,14 @@
 import { Component, createSignal, For } from "solid-js";
+import JSRLMap from "../playlistMap.json";
 
 export const JSRLiveImport: Component = () => {
-    
-
-    return(
-        <div>
-            André
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul>
+        <For each={Object.keys(JSRLMap)}>
+          {(playlistName) => <li>{playlistName}</li>}
+        </For>
+      </ul>
+    </div>
+  );
+};
