1f49073403088013dcb4d2cd5b425e9c9da36673
Lost-in-Static <32521625+Lost-in-Static@users.noreply.github.com>
2023-04-13 16:15:17 +0000

    Add [Symbol.iterator]() to Playlist

--- src/types/Map.ts
+++ src/types/Map.ts
@@ -7,10 +7,11 @@ export type Track = {
 export type Playlist = {
     name: string
     tracks: Array<Track>
+    [Symbol.iterator](): IterableIterator<Track>
 }
 
 export type TrackData = {
     playlists: Array<Playlist>
 }
 
-export type PlaylistMap = Record<string, Track>
+export type PlaylistMap = Record<string, Playlist>
